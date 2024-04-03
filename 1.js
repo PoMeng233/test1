4ab33630fe380b4b33e8fcfa8a3872a7b0f5365a
pomeng <pomengqaq@gmail.com>
2024-04-03 13:51:02 +0800

    2222

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+// ==UserScript==
+// @name         test1
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  a test for learning
+// @author       You
+// @match        https://www.tampermonkey.net/index.php?ext=gcal&version=5.1.6193
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world');
+})();
